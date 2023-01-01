b54d80385e1010b17bfc4561085e80a97e1a4949
Dov_Dembin <Dov.Dembin@Dell.com>
2023-01-01 20:58:59 +0200

    ss

--- vars/releasenotes.groovy
+++ vars/releasenotes.groovy
@@ -1,26 +1,30 @@
-import java.io.*;
-import groovy.io.*;
+import groovy.io.FileType;
+import java.io.File;
+import java.text.SimpleDateFormat
 
 @NonCPS
-def call(Map config=[:]) {
-    def dir = new File(pwd());
-
-    new File(dir.path + '\\releasenotes.txt').withWriter('utf-8')
-    { 
-        def dir = new File(pwd());
+def call(Map config=[:]){
+	def dir = new File(pwd());
     
-        new File(dir.path + '/releasenotes.txt').withWriter('utf-8') 
-        { 
-            writer -> 
-            dir.eachFileRecurse(FileType.ANY){ file ->
-            if (file.isDirectory()){
-                writer.writeLine(file.name);            
-            }
-            else
-            {
-                writer.writeLine('\t' + file.name + '\t' + file.length());
-            }
-            } 
-        }
+	new File(dir.path + '/releasenotes.txt').withWriter('utf-8') 
+	{ 
+		writer -> 
+	    dir.eachFileRecurse(FileType.ANY){ file ->
+		if (file.isDirectory()){
+		    writer.writeLine(file.name);            
+		}
+		else
+		{
+		    writer.writeLine('\t' + file.name + '\t' + file.length());
+		}
+		} 
+	}
+    
+	def date = new Date()
+	def sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss")
+	echo "Date and Time IS: " + sdf.format(date)
+    
+    if (config.changes != "false"){
+    	echo "changes";
     }
 }
