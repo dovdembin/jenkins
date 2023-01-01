40f3d7e53419b425583d1a519a317636f3b91c75
Dov_Dembin <Dov.Dembin@Dell.com>
2023-01-01 22:34:03 +0200

    dd

--- vars/releasenotes.groovy
+++ vars/releasenotes.groovy
@@ -4,21 +4,7 @@ import java.text.SimpleDateFormat
 
 @NonCPS
 def call(Map config=[:]){
-	def dir = new File(pwd());
-    
-	new File(dir.path + '/releasenotes.txt').withWriter('utf-8') 
-	{ 
-		writer -> 
-            dir.eachFileRecurse(FileType.ANY){ file ->
-            if (file.isDirectory()){
-                writer.writeLine(file.name);            
-            }
-            else
-            {
-                writer.writeLine('\t' + file.name + '\t' + file.length());
-            }
-		} 
-	}
+	 println("koko")
     
 	
 }
