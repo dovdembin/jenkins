3f5314a8717bcad03e4d73b043ae1840e5d15280
Dov_Dembin <Dov.Dembin@Dell.com>
2023-01-01 20:58:15 +0200

    ss

--- vars/releasenotes.groovy
+++ vars/releasenotes.groovy
@@ -7,15 +7,20 @@ def call(Map config=[:]) {
 
     new File(dir.path + '\\releasenotes.txt').withWriter('utf-8')
     { 
-        // writer ->
-        //     dir.eachFileRecurse(FileType.ANY) { file ->
-        //         if (file.isDirectory() ) {
-        //             writer.writeLine(file.name);
-        //         }
-        //         else
-        //         {
-        //             writer.writeLine('\t' + file.name + '\t' + file.length());
-        //         }
-        //     }
+        def dir = new File(pwd());
+    
+        new File(dir.path + '/releasenotes.txt').withWriter('utf-8') 
+        { 
+            writer -> 
+            dir.eachFileRecurse(FileType.ANY){ file ->
+            if (file.isDirectory()){
+                writer.writeLine(file.name);            
+            }
+            else
+            {
+                writer.writeLine('\t' + file.name + '\t' + file.length());
+            }
+            } 
+        }
     }
 }
