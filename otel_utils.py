40685f1192c78e7a5389c2affff1b308ce9e80ef
Dov_Dembin <Dov.Dembin@Dell.com>
2023-05-28 22:39:01 +0300

    ss

--- otel_utils.py
+++ otel_utils.py
@@ -1,3 +1,5 @@
+import sys
+
 def method1(arg1, arg2):
     # Implement the logic for method1
     print(f"Executing method1 with arguments: {arg1}, {arg2}")
@@ -10,10 +12,16 @@ def method3():
     # Implement the logic for method3
     print("Executing method3")
 
-# Define a dictionary mapping method names to functions
-methods = {
-    "method1": method1,
-    "method2": method2,
-    "method3": method3
-}
+# Get the method name and arguments from command-line arguments
+method_name = sys.argv[1]
+args = sys.argv[2:]
 
+# Call the appropriate method based on the provided method name
+if method_name == "method1":
+    method1(*args)
+elif method_name == "method2":
+    method2(*args)
+elif method_name == "method3":
+    method3()
+else:
+    print(f"Unknown method: {method_name}")
