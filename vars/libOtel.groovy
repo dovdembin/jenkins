b4cd1f2d79bc391630363e2b88a6644c3f19a8af
Dov_Dembin <Dov.Dembin@Dell.com>
2023-05-30 00:32:01 +0300

    s

--- vars/libOtel.groovy
+++ vars/libOtel.groovy
@@ -1,3 +1,5 @@
+LABJUNGLE_URL = "http://labjungle.devops.xiodrm.lab.emc.com"
+LABJUNGLE_KEY = "9703aa016d613b2b21bbb0e6833c3078c811a5d1"
 
 
 
@@ -12,8 +14,6 @@ def getLabels(String str) {
 }
 
 def getIntersection(String lables, String rig) {
-	def LABJUNGLE_URL = "http://labjungle.devops.xiodrm.lab.emc.com"
-	def LABJUNGLE_KEY = "9703aa016d613b2b21bbb0e6833c3078c811a5d1"
 	def labJungle_api="${LABJUNGLE_URL}/api/v1/cluster/?api_key=${LABJUNGLE_KEY}"
 	def res = sh(script:"""
 							curl -s --location '${labJungle_api}&name=${rig}' | jq  '.objects[].tags'
