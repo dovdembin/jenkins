fa399742978e59922ab1f0e49c345dddb36c09e2
Dov_Dembin <Dov.Dembin@Dell.com>
2023-05-30 00:28:55 +0300

    e

--- Core_Template.groovy
+++ Core_Template.groovy
@@ -6,8 +6,8 @@ node {
     stage('Preparation') { // for display purposes
          
 
-        def labels = "MLK-EX1\\|MLK-EX2\\|MLK-EX3\\|MLK-EX4,PhysicalLG"
-        def rig = "WK-H2686"
+        def labels = "MLK-EX1\|MLK-EX2\|MLK-EX3\|MLK-EX4,PhysicalLG"
+        def rig = "WK-D0089"
         def labels_separator = libOtel.getLabels("-l ${labels}")
 
         def res = libOtel.getIntersection(labels_separator, rig)
