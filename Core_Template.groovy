8b13eabefdb4ed7b20bf972175bd8b29da32b19c
Dov_Dembin <Dov.Dembin@Dell.com>
2023-05-30 03:01:51 +0300

    s

--- Core_Template.groovy
+++ Core_Template.groovy
@@ -8,8 +8,10 @@ node {
 
         def labels = "MLK-EX1\\|MLK-EX2\\|MLK-EX3\\|MLK-EX4,PhysicalLG"
         def rig = "WK-D0089"
+
 	    def labJungle_api="${LABJUNGLE_URL}/api/v1/cluster/?api_key=${LABJUNGLE_KEY}"
-        def cmd = "curl -s --location '${labJungle_api}&name=${rig}' | jq  '.objects[].tags'"
+        def cmd = "curl -s --location '${labJungle_api}&name=${rig}'"
+        def cmd_tags = cmd + " | jq  '.objects[].tags'" 
         def tags = sh(script: cmd, returnStdout: true, label: "xpool_allocation")
         def labels_separator = libOtel.getLabels("-l ${labels}")
 
