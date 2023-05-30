bf93b841744889130d29e9abfe6b3ca391627356
Dov_Dembin <Dov.Dembin@Dell.com>
2023-05-30 03:22:36 +0300

    d

--- vars/libOtel.groovy
+++ vars/libOtel.groovy
@@ -13,3 +13,21 @@ def getIntersection(String lables, String tags) {
 	ArrayList array2 = lables.split(",")
 	return array1.intersect(array2)
 }
+
+def checkLabels(String text, String appliance) {
+	def pattern = /([A-Z][A-Z]-[A-Z]\d\d\d\d)-([A-Z][A-Z]-[A-Z]\d\d\d\d)-.*/
+	def m1
+	def m2
+	if(appliance ==~ pattern){
+		def (res1) = appliance =~ pattern
+		m1 = res1[1]
+		m2 = res1[2]
+	}
+	else {
+		return getLabels(text, appliance)
+	}
+	ArrayList list1 = getLabels(text, m1).split(",")
+	ArrayList list2 = getLabels(text, m2).split(",")
+	def createdList = list1 + list2
+	return createdList.unique().join(",")
+}
