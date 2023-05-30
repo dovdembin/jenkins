b16dd03c6269f3f1ef384c4c384a875acefd5105
Dov_Dembin <Dov.Dembin@Dell.com>
2023-05-30 03:57:54 +0300

    s

--- vars/libOtel.groovy
+++ vars/libOtel.groovy
@@ -15,14 +15,14 @@ def getIntersection(String lables, String tags) {
 }
 
 def getGeneration(String appliance) {
-	def cmd = "curl -s --location '${LABJUNGLE_URL}/api/v1/cluster/?api_key=${LABJUNGLE_KEY}&name=${appliance}"
+	def cmd = "curl -s --location '${LABJUNGLE_URL}/api/v1/cluster/?api_key=${LABJUNGLE_KEY}&name=${appliance}'"
 	cmd = cmd + ' | jq  .objects[].generation.name'
 	def generation_name = sh(script: cmd, returnStdout: true, label: "xpool_allocation")
 	return generation_name
 }
 
 def getTags(String appliance) {
-	def cmd = "curl -s --location '${LABJUNGLE_URL}/api/v1/cluster/?api_key=${LABJUNGLE_KEY}&name=${appliance}"
+	def cmd = "curl -s --location '${LABJUNGLE_URL}/api/v1/cluster/?api_key=${LABJUNGLE_KEY}&name=${appliance}'"
 	cmd = cmd + ' | jq  .objects[].tags'
 	def tags = sh(script: cmd, returnStdout: true, label: "xpool_allocation")
 	return tags
