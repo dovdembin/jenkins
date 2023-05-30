2936e507c5153c326aa7046f34bea6cde0281fc3
Dov_Dembin <Dov.Dembin@Dell.com>
2023-05-30 04:04:10 +0300

    s

--- Core_Template.groovy
+++ Core_Template.groovy
@@ -6,11 +6,13 @@ node {
     stage('Preparation') { // for display purposes
          
 
-        def labels = "MLK-EX1\\|MLK-EX2\\|MLK-EX3\\|MLK-EX4,PhysicalLG"
-        def appliance = "WK-D0089"
+        // def labels = "MLK-EX1\\|MLK-EX2\\|MLK-EX3\\|MLK-EX4,PhysicalLG"
+        // def appliance = "WK-D0089"
+        def labels = "EX\\|MLK\\|Riptide,CopperBlade\\|LightBlade"
+        def appliance = "RT-D0201-RT-D0197-federation-TAG"
         def pattern = /([A-Z][A-Z]-[A-Z]\d\d\d\d)-([A-Z][A-Z]-[A-Z]\d\d\d\d)-.*/
         if(appliance ==~ pattern) {
-            
+
         } else {
             def tags = libOtel.getTags(appliance)
             def labels_separator = libOtel.getLabels("-l ${labels}")
