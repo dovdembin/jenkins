b8377690d312d25a18b9473ffa1577595aaea2c1
Dov_Dembin <Dov.Dembin@Dell.com>
2023-05-31 13:52:08 +0300

    ss

--- vars/libOtel.groovy
+++ vars/libOtel.groovy
@@ -1,3 +1,4 @@
+@NonCPS
 def getLabels(String str) {
 	def pattern = /.*-l\s(.*)/
 	if(str ==~ pattern) {
@@ -8,6 +9,7 @@ def getLabels(String str) {
 	}
 }
 
+@NonCPS
 def getTags(String appliance) {
 	def cmd = "curl -s --location '${LABJUNGLE_URL}/api/v1/cluster/?api_key=${LABJUNGLE_KEY}&name=${appliance}'"
 	cmd = cmd + ' | jq  .objects[].tags'
@@ -15,23 +17,22 @@ def getTags(String appliance) {
 	return tags
 }
 
+@NonCPS
 def getIntersection(String lables, String tags) {
 	ArrayList array1 = tags.split(",")
 	ArrayList array2 = lables.split(",")
 	return array1.intersect(array2)
 }
 
+@NonCPS
 def getListTags(String labels, String appliance) {
 	def labelsWithCommas = getLabels(labels)
 	def pattern = /([A-Z][A-Z]-[A-Z]\d\d\d\d)-([A-Z][A-Z]-[A-Z]\d\d\d\d)-.*/
 	if(appliance ==~ pattern){
 		def (res1) = appliance =~ pattern
-		ArrayList arr = res1
-		def m1 = arr[1]
-		def m2 = arr[2]
-		def tags1 = getTags(m1)
+		def tags1 = getTags(res1[1])
         def intersection1 = getIntersection(labelsWithCommas, tags1)
-		def tags2 = getTags(m2)
+		def tags2 = getTags(res1[2])
         def intersection2 = getIntersection(labelsWithCommas, tags2)
 		ArrayList combine = intersection1 + intersection2
 		return combine.join(",")
@@ -42,6 +43,7 @@ def getListTags(String labels, String appliance) {
 	}
 }
 
+@NonCPS
 def getGeneration(String appliance) {
 	def cmd = "curl -s --location '${LABJUNGLE_URL}/api/v1/cluster/?api_key=${LABJUNGLE_KEY}&name=${appliance}'"
 	cmd = cmd + ' | jq  .objects[].generation.name'
@@ -49,14 +51,12 @@ def getGeneration(String appliance) {
 	return generation_name
 }
 
+@NonCPS
 def getListGenertions(String appliance) {
 	def pattern = /([A-Z][A-Z]-[A-Z]\d\d\d\d)-([A-Z][A-Z]-[A-Z]\d\d\d\d)-.*/
 	if(appliance ==~ pattern){
 		def (res1) = appliance =~ pattern
-		ArrayList arr = res1
-		def m1 = arr[1]
-		def m2 = arr[2]
-		return [getGeneration(m1),getGeneration(m2)].join(",")
+		return [getGeneration(res1[1]),getGeneration(res1[2])].join(",")
 	} else {
 		return getGeneration(appliance)
 	}
