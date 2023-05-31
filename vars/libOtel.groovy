c148bd739254ea7567b5ba973742fddd58540f36
Dov_Dembin <Dov.Dembin@Dell.com>
2023-05-31 18:16:11 +0300

    dd

--- vars/libOtel.groovy
+++ vars/libOtel.groovy
@@ -1,86 +1,64 @@
 
 def getLabels(String str) {
-	try { 
-		def pattern = /.*-l\s(.*)/
-		if(str ==~ pattern) {
-			def (word1) = str =~ pattern
-			return word1[1].toString().replaceAll(/\\\|/, ",").trim()
-		}
-	} catch(Exception  e1) {
-		println("ERROR: in libOtel getLabels")
+	def pattern = /.*-l\s(.*)/
+	if(str ==~ pattern) {
+		def (word1) = str =~ pattern
+		return word1[1].toString().replaceAll(/\\\|/, ",").trim()
+	} else {
+		return "noMatch"
 	}
 }
 
 
 def getTags(String appliance) {
-	try {
-		def cmd = "curl -s --location '${LABJUNGLE_URL}/api/v1/cluster/?api_key=${LABJUNGLE_KEY}&name=${appliance}'"
-		cmd = cmd + ' | jq  .objects[].tags'
-		def tags = sh(script: cmd, returnStdout: true, label: "getTags")
-		return tags
-	} catch(Exception  e1) {
-		println(e1)
-	}
+	def cmd = "curl -s --location '${LABJUNGLE_URL}/api/v1/cluster/?api_key=${LABJUNGLE_KEY}&name=${appliance}'"
+	cmd = cmd + ' | jq  .objects[].tags'
+	def tags = sh(script: cmd, returnStdout: true, label: "getTags")
+	return tags
 }
 
 
 def getIntersection(String lables, String tags) {
-	try {
-		ArrayList array1 = tags.split(",")
-		ArrayList array2 = lables.split(",")
-		return array1.intersect(array2)
-	} catch(Exception  e1) {
-		println("ERROR: in libOtel getIntersection")
-	}
+	ArrayList array1 = tags.split(",")
+	ArrayList array2 = lables.split(",")
+	return array1.intersect(array2)
 }
 
 
 def getListTags(String labels, String appliance) {
-	try {
-		def labelsWithCommas = getLabels(labels)
-		
-		if(appliance.length() > 8) {
-			m1 = appliance.substring(0,8)
-			m2 = appliance.substring(9,17)
-			def tags1 = getTags(m1)
-			def intersection1 = getIntersection(labelsWithCommas, tags1)
-			def tags2 = getTags(m2)
-			def intersection2 = getIntersection(labelsWithCommas, tags2)
-			ArrayList combine = intersection1 + intersection2
-			return combine.join(",")
-		} else {
-			def tags = getTags(appliance)
-			def intersection = getIntersection(labelsWithCommas, tags)
-			return intersection.join(",")
-		}
-	} catch(Exception  e1) {
-		println("ERROR: in libOtel getListTags")
+	def labelsWithCommas = getLabels(labels)
+
+	if(appliance.length() > 8) {
+		m1 = appliance.substring(0,8)
+		m2 = appliance.substring(9,17)
+		def tags1 = getTags(m1)
+        def intersection1 = getIntersection(labelsWithCommas, tags1)
+		def tags2 = getTags(m2)
+        def intersection2 = getIntersection(labelsWithCommas, tags2)
+		ArrayList combine = intersection1 + intersection2
+		return combine.join(",")
+	} else {
+		def tags = getTags(appliance)
+        def intersection = getIntersection(labelsWithCommas, tags)
+        return intersection.join(",")
 	}
 }
 
 
 def getGeneration(String appliance) {
-	try {
-		def cmd = "curl -s --location '${LABJUNGLE_URL}/api/v1/cluster/?api_key=${LABJUNGLE_KEY}&name=${appliance}'"
-		cmd = cmd + ' | jq  .objects[].generation.name'
-		def generation_name = sh(script: cmd, returnStdout: true, label: "getGeneration")
-		return generation_name.trim()
-	} catch(Exception  e1) {
-		println("ERROR: in libOtel getGeneration")
-	}
+	def cmd = "curl -s --location '${LABJUNGLE_URL}/api/v1/cluster/?api_key=${LABJUNGLE_KEY}&name=${appliance}'"
+	cmd = cmd + ' | jq  .objects[].generation.name'
+	def generation_name = sh(script: cmd, returnStdout: true, label: "getGeneration")
+	return generation_name.trim()
 }
 
 
 def getListGenertions(String appliance) {
-	try {
-		if(appliance.length() > 8) {
-			m1 = appliance.substring(0,8)
-			m2 = appliance.substring(9,17)
-			return [getGeneration(m1),getGeneration(m2)].join(",")
-		} else {
-			return getGeneration(appliance)
-		}
-	} catch(Exception  e1) {
-		println("ERROR: in libOtel getGeneration")
+	if(appliance.length() > 8) {
+		m1 = appliance.substring(0,8)
+		m2 = appliance.substring(9,17)
+		return [getGeneration(m1),getGeneration(m2)].join(",")
+	} else {
+		return getGeneration(appliance)
 	}
 }
